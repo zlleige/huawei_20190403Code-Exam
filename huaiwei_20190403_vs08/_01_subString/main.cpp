405fc71502e0f29d32f622b9715af6b675d82f2c
zlleige <2506078824@qq.com>
2019-04-16 21:57:57 +0800

    完成部分第一题的操作

--- huaiwei_20190403_vs08/_01_subString/main.cpp
+++ huaiwei_20190403_vs08/_01_subString/main.cpp
@@ -4,8 +4,8 @@
 会删除掉，如果改行不足固定长度或者为空，则取出剩余部分的内容到新
 的数组中，继续下一行。
 输入描述：第一行是每次读取的固定长度
-          第2-n行是需要合并的数组，不同的数组用换行符分隔，数组
-	  内用逗号分隔。
+第2-n行是需要合并的数组，不同的数组用换行符分隔，数组
+内用逗号分隔。
 输出描述：输出一个新的数组，用逗号分隔。
 样例
 输入：
@@ -18,137 +18,96 @@
 ***/
 
 #include <iostream>
-#include <string>
 #include <vector>
-#include <list>
-#include <iterator>
-#include <algorithm>
+#include <string>
 #include <sstream>
+#include <cstdlib>  //atoi()
 
 using namespace std;
-void coutVector(vector<vector<int>> &v);
-void coutVector(list<list<int>> &v);
-void coutVector(vector<int> &v);
-void coutVector(list<int> &v);
 
-void coutVector(vector<vector<int>> &v)
+//打印数据
+void PrintVector( vector<vector<int>>  &vDate,string slip)
 {
-	if (v.empty())
-		cout << "vector is empty!" << endl;
-	vector<vector<int>>::iterator lineIter;
-	vector<int>::iterator rowIter;
-	vector<int> temp;
-	for (lineIter = v.begin(); lineIter != v.end(); ++lineIter)
+	for(vector<vector<int>>::iterator v_it=vDate.begin();v_it!=vDate.end();v_it++)
 	{
-		temp = *lineIter;
-		for (rowIter = temp.begin(); rowIter != temp.end(); ++rowIter)
-			cout << *rowIter << " ";
-		cout << endl;
+		for(vector<int>::iterator it=v_it->begin();it!=v_it->end();it++)
+		{
+			cout << *it<<slip;
+		}
+		cout <<endl;
 	}
-}
-void coutVector(list<list<int>> &v)
-{
-	if (v.empty())
-		cout << "vector is empty!" << endl;
-	list<list<int>>::iterator lineIter;
-	list<int>::iterator rowIter;
-	list<int> temp;
-	for (lineIter = v.begin(); lineIter != v.end(); ++lineIter)
-	{
-		temp = *lineIter;
-		for (rowIter = temp.begin(); rowIter != temp.end(); ++rowIter)
-			cout << *rowIter << " ";
-		cout << endl;
-	}
-}
-void coutVector(vector<int> &v)
-{
-	if (v.empty())
-		cout << "vector is empty!" << endl;
-	vector<int>::iterator iter;
-	for (iter = v.begin(); iter != v.end(); ++iter)
-		cout << *iter << " ";
-	cout << endl;
-}
-void coutVector(list<int> &v)
-{
-	if (v.empty())
-		cout << "vector is empty!" << endl;
-	list<int>::iterator iter;
-	for (iter = v.begin(); iter != v.end(); ++iter)
-		cout << *iter << ",";
-	cout << endl;
 }
 
-int main()
+//打印数据
+void PrintVector( vector<vector<int>>  &vDate)
 {
-	list<list<int>> data;//存放输入的数组
-	list<int> row;      //存放输入的行数组
-	list<int> output;	//输出链表	
-	list<list<int>>::iterator lineIter;
-	list<list<int>>::iterator tempIter;
-	list<int>::iterator rowIter;
-	int value;
-	int n;
-	cin >> n;
-	cin.get();
-	//cout << n << endl;
-	while (cin)
+	int group=vDate.size(); //获得输入的数据组数
+	if(group <= 0) return;
+	bool isOver=
+	for(int i=0;i< group;i++) 
 	{
-		cin >> value;
-		row.push_back(value);
-		if (cin.get() == '\n')
-		{
-			data.push_back(row);
-			row.clear();
-		}
+
 	}
-	int i = 0;
-	while (!data.empty())
+	for(vector<vector<int>>::iterator v_it=vDate.begin();v_it!=vDate.end();v_it++)
 	{
-		for (lineIter = data.begin(); lineIter != data.end(); ++lineIter)
+		for(vector<int>::iterator it=v_it->begin();it!=v_it->end();it++)
 		{
-			if (lineIter == data.end())//迭代器到达末尾，退出循环
-			{
-				cout << "break" << endl;
-				break;
-			}
-			if (i == n || i == 0)//计数已达n，则归零，初始化时也归零
-				i = 0;
-			if ((*lineIter).empty())			//判断行是否为空
-			{									//此时迭代器为头部，清除元素后，退出循环
-				if (lineIter == data.begin())
-				{
-					data.erase(lineIter);
-					//cout << "empty.begin" << endl;
-					break;
-				}
-				else							// 若不为头部，则暂存当前迭代器的前一个位置，将当前元素删除，
-				{								// 迭代器赋值为前迭代器的前一个位置，进入下一个循环
-					tempIter = (--lineIter);
-					data.erase(++lineIter);
-					lineIter = tempIter;
-					//cout << "empty.other" << endl;
-					continue;
-				}
-			}
-			for (rowIter = (*lineIter).begin(); rowIter != (*lineIter).end() && i < n; ++i)
-			{
-				output.push_back(*rowIter);
-				//cout << "i= " << i << " value= " << *rowIter << endl;
-				(*lineIter).pop_front();
-				if (!(*lineIter).empty())			//删除首元素后，行不为空，获取新list的首元素，进入下一个循环
-					rowIter = (*lineIter).begin();
-				else
-				{									//删除首元素后，行为空,计数加1，退出循环
-					++i;
-					break;
-				}
-			}
-			//cout << "output : ";
-			//coutVector(output);
+			cout << *it<<slip;
 		}
+		cout <<endl;
 	}
-	coutVector(output);
+}
+
+int main() 
+{
+	//输入固定长度
+	int count=0;
+	cin >> count;
+	//接受输入字符串 
+	string strInput,strTemp; 
+	vector<vector<int>>  vDate; //存所有输入数组
+	vector<int> vTemp; //存当前输入的一条数据
+	while(getline(cin,strInput))
+	{
+		if(!strInput.empty())
+		{
+			vTemp.clear();
+			//分割字符串，得到整数
+			stringstream  input(strInput);
+			while(getline(input,strTemp,',')) vTemp.push_back(atoi(strTemp.c_str())); //strTemp存数据
+			vDate.push_back(vTemp);
+			
+			PrintVector(vDate," "); //测试打印所有数据
+			//打印输出
+			
+			
+		}
+			
+	}
+	system("pause");
 	return 0;
 }
+
+
+/****
+
+string raw_data("3, 4, 5, 76"), tmp;
+vector<string> data;
+stringstream input(raw_data);
+
+while (getline(input, tmp, ',')) data.push_back(tmp);
+for (vector<string>::iterator it=data.begin();it!= data.end();it++)  cout << atoi((*it).c_str()) << endl;
+
+
+void split(const string& s,vector<int>& sv,const char flag = ' ') {
+sv.clear();
+istringstream iss(s);
+string temp;
+
+while (getline(iss, temp, flag)) {
+sv.push_back(stoi(temp));
+}
+return;
+}
+*/
+
